bce37953771bca43fb5d959b2deb875f72bef6e7
Josh Gordon <joshua.sam.gordon@gmail.com>
2017-01-16 16:37:10 -0500

    Auto Command For Spline
    This follows a path

--- src/org/usfirst/frc/team1100/robot/commands/AutoSplineFollowCommand.java
+++ src/org/usfirst/frc/team1100/robot/commands/AutoSplineFollowCommand.java
@@ -50,6 +50,7 @@ public class AutoSplineFollowCommand extends Command {
 	// Called once after isFinished returns true
 	@Override
 	protected void end() {
+		
 	}
 
 	// Called when another command which requires one or more of the same
