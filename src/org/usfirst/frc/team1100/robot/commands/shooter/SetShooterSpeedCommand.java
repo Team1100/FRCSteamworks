6266cf9329edff1d168661dacd8b83deef00865c
Thomas Hayden <supremesteak1@gmail.com>
2017-01-21 11:03:00 -0500

    ChangedisFinished in Set[Shooter|Augar|Intake]SpeedCommand

--- src/org/usfirst/frc/team1100/robot/commands/shooter/SetShooterSpeedCommand.java
+++ src/org/usfirst/frc/team1100/robot/commands/shooter/SetShooterSpeedCommand.java
@@ -38,7 +38,7 @@ public class SetShooterSpeedCommand extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
